759c2e3ca18d5227590dfc01383f69c8f6425eed
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-11-28 23:17:24 -0300

    mas seederes

--- example-app/database/seeders/PerrosTableSeeder.php
+++ example-app/database/seeders/PerrosTableSeeder.php
@@ -37,5 +37,77 @@ class PerrosTableSeeder extends Seeder
             'url_foto' => 'otra_url_de_la_foto.jpg',
             'descripcion' => 'perro guaton',
         ]);
+
+        Perro::create([
+            'nombre' => 'Maxo',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'le encanta correr y jugar al aire libre',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Luna',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'siempre en busca de mimos y atencion',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Rocky',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'le encanta explorar nuevos lugares',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Bellaila',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => ' disfruta de largos paseos y momentos tranquilos',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Daisy',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'siempre está listo para aprender nuevos trucos',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Milo',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'es el compañero perfecto para momentos de relajacion',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Zoe',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'encuentra diversión en todo lo que hace',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Dexter',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'siempre lista para nuevas experiencias',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Ruby',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => ' le gusta resolver rompecabezas y desafíos mentales',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Dogo',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'siempre cuidando de su familia con devoción',
+        ]);
+
+        Perro::create([
+            'nombre' => 'Java',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'perro no tan guaton',
+        ]);
+
+        Perro::create([
+            'nombre' => 'cpp',
+            'url_foto' => 'otra_url_de_la_foto.jpg',
+            'descripcion' => 'perro programador',
+        ]);
     }
 }
